05578b6cda96a40b9b538cf580ab131eb40250e4
Nathan Pelletier <npelleti@ualberta.ca>
2022-10-20 17:31:58 -0600

    Moved Hello world to repository

---
+++ Hello world/hello world.js
@@ -0,0 +1,26 @@
+/** hello world.html 
+ *  by Nathan Pelletier
+ *  August 12 2022
+
+ * This is my first time working in html & javascript
+ * This program should be a simple button that displays 
+ * Hello World to console when pressed--> 
+ */
+ 
+alert("I'm here")
+//class hello_world{
+	function main(){
+		console.log("Hello World");
+	}//main
+//}//hello_world
+
+
+//let new_world = new hello_world();
+//new_world.main();
+
+/** KEY NOTES
+ * Javascript interacts with html using methods
+ * javascript can interact with itself using class objects
+ *  Unless further research turns up otherwise, be sure to
+ *  use functions only when communicating with html.
+ */
