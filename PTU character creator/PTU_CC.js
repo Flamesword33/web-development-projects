10d5fa5d9c793a220aa903fb7eba2232cd5096b7
Nathan Pelletier <npelleti@ualberta.ca>
2022-10-20 17:32:21 -0600

    Moved PTU character creator to repository

---
+++ PTU character creator/PTU_CC.js
@@ -0,0 +1,13 @@
+/** Starfinder_CC.js 
+ *  by Nathan Pelletier
+ *  Started: August 12 2022
+
+ * A simple character creation form online for PTU(Pokemon Table top United)
+ * 
+ * Stat buttons with + or - 1
+ * Hyperlinks to each of the stats
+ */
+
+function main(){
+    
+}//main
