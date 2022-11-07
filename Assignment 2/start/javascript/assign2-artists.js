15b066a609f429c8a5a78b47fa70d9a2e54a4cac
Nathan Pelletier <npelleti@ualberta.ca>
2022-11-06 18:05:37 -0700

    Added output_artist function to write a block of html code

--- Assignment 2/start/javascript/assign2-artists.js
+++ Assignment 2/start/javascript/assign2-artists.js
@@ -1,3 +1,14 @@
+function output_artist(picture, artist_name, num_of_sales){
+    document.write('<div class="artist">');
+    document.write('<img src="../images/artists/'+ picture +'.jpg">');
+    document.write('<p>'+ artist_name +'</p>');
+    document.write('<p><a href="#"><span class="glyphicon glyphicon-fire"></span>');
+    document.write('Sales <span class="how_many_sales">' + num_of_sales + '</span>');
+    document.write('</a></p>');
+    document.write('</div> <!--artist-->');
+    document.write('</div> <!--art_by_artist-->');
+    document.write('');
+}//output_artist
 
     var file_names = ["1","2","17","19", "21", "29", "95", "97", "98", "99", "101", "106"];
     var title = ["Picasso","Raphael","Van Gogh", "Botticelli", "Klimt", "Matisse", 
