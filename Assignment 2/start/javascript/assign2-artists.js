2131b5708e83c6b4b58873ab6ee6d703f7da8a3d
Nathan Pelletier <npelleti@ualberta.ca>
2022-11-06 18:05:03 -0700

    Added data to be added into html file

--- Assignment 2/start/javascript/assign2-artists.js
+++ Assignment 2/start/javascript/assign2-artists.js
@@ -1,1 +1,5 @@
 
+    var file_names = ["1","2","17","19", "21", "29", "95", "97", "98", "99", "101", "106"];
+    var title = ["Picasso","Raphael","Van Gogh", "Botticelli", "Klimt", "Matisse", 
+    "Michelangelo", "Vermeer", "da Vinci", "Cezanne", "Monet", "Caravaggio"];
+    var num_of_sales = [66, 56, 40, 37, 36, 34, 32, 31, 26, 25, 25, 18];
