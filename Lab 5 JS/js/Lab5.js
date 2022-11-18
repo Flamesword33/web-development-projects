6660948561ab56a5a98c9d85382d1425d7c75aea
Nathan Pelletier <npelleti@ualberta.ca>
2022-11-18 00:18:05 -0700

    Removed debug logs as I figured out the debugger

--- Lab 5 JS/js/Lab5.js
+++ Lab 5 JS/js/Lab5.js
@@ -34,20 +34,13 @@ function overwrite_large_image(small_image, small_title){
 
 //===event listeners===//
 window.addEventListener("load", function(event) {
-    console.log(event);
+    
     //for thumbnails
     var thumbnails = document.querySelectorAll("#thumbnails img");
-/*  console.log(thumbnails);
-    console.log(thumbnails[0]); */
     for(var i=0; i < thumbnails.length; i++){
         var title = thumbnails[i].getAttribute("title");
         var img_path = thumbnails[i].getAttribute("src");
         var img = img_path.split("/");
-        
-/*      console.log("title: "+ title);
-        console.log("path: " + img_path);
-        console.log("image: " + img[2]); */
-
         thumbnails[i].addEventListener("click", replace_large_image(img[2], title));
 }//for
 
