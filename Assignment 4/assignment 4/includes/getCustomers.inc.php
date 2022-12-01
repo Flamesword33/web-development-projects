26f07201f031b703a0f1ea43c84ffcfe378e78bc
Nathan Pelletier <npelleti@ualberta.ca>
2022-12-01 12:58:22 -0700

    Small problem documented: special characters in data don't display

--- Assignment 4/assignment 4/includes/getCustomers.inc.php
+++ Assignment 4/assignment 4/includes/getCustomers.inc.php
@@ -1,6 +1,8 @@
 <!--This file reads customer.txt and gets:
 		Their name, University, City and Sales
-	It then displays these details in table form-->	Small problem: one last name contains a special character and it doesn't display-->
+	It then displays these details in table form
+
+	Small problem: one last name contains a special character and it doesn't display-->
 
 <script type="text/javascript">
 	$(function() {
