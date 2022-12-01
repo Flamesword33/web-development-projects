0d746b0f3c375bf7cb40bacf5061cf059aa2c89e
Nathan Pelletier <npelleti@ualberta.ca>
2022-12-01 12:46:11 -0700

    Designed HTML base code using example row

--- Assignment 4/assignment 4/includes/getCustomers.inc.php
+++ Assignment 4/assignment 4/includes/getCustomers.inc.php
@@ -11,3 +11,11 @@
 </script>  
 
 <!-- TEMPLATE FOR ROWS
+<tr>
+	<td><a href="chapter12-project3.php?customer=2">Leonie Kohler</a></td>
+	<td>University of Stuttgart</td>
+	<td>Stuttgart</td>
+	<td><span class="sparkline">1,4,4,7,5,9,10,10,10,12,11,9</span></td>
+</tr>
+-->
+
