3636a2e2a17d277ea704822657629ae54f434dbc
Nathan Pelletier <npelleti@ualberta.ca>
2022-12-01 12:45:28 -0700

    Copied critical code from chapter12-project3.php to make sparkline work

--- Assignment 4/assignment 4/includes/getCustomers.inc.php
+++ Assignment 4/assignment 4/includes/getCustomers.inc.php
@@ -1,3 +1,13 @@
 <!--This file reads customer.txt and gets:
 		Their name, University, City and Sales
-	It then displays these details in table form-->
+	It then displays these details in table form-->	Small problem: one last name contains a special character and it doesn't display-->
+
+<script type="text/javascript">
+	$(function() {
+		/** This code runs when everything has been loaded on the page */
+		/* Inline sparklines take their values from the contents of the tag */
+		$('.sparkline').sparkline('html', {type: 'bar', barColor: '#6200EA'}); 
+	});
+</script>  
+
+<!-- TEMPLATE FOR ROWS
