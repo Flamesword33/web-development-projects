0531873cdbe72ab18cae17fe32bc8e97076692cf
Nathan Pelletier <npelleti@ualberta.ca>
2022-12-26 16:47:18 -0700

    Wrote basic project header notes for both files

--- 2023/list sort/js/list-sort.js
+++ 2023/list sort/js/list-sort.js
@@ -1,2 +1,13 @@
 /** 
+ * list-sort.js
+ * by Nathan Pelletier
+ * Started: December 26, 2022
+ * 
+ * Little idea for a stand alone library:
+ *   When user clicks on a class="sortable" element the list reforms, sorted alphabetically.
+ *    Probably changes class to class = "sort-down"
+ *    Change all other classes of sort-down or sort-up to sortable
+ *    If the user clicks again the list sorts reverse alphabetically.
+ *    Probably change class to class = "sort-up"
+ *    3rd click on same element returns list to generic sorting based on data.
 */
