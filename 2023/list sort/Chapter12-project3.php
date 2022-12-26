cf9cbe979ba44561da713376ca6aed2edbbd9182
Nathan Pelletier <npelleti@ualberta.ca>
2022-12-26 16:46:25 -0700

    Created list-sort.js & list-sort.css and linked both of them in main php file

--- 2023/list sort/Chapter12-project3.php
+++ 2023/list sort/Chapter12-project3.php
@@ -178,12 +178,14 @@
   <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue_grey-orange.min.css">
 
   <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/list-sort.css">
     
     
   <script   src="https://code.jquery.com/jquery-1.7.2.min.js" ></script>
        
   <script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
   <script src="js/jquery.sparkline.2.1.2.js"></script>
+  <script src="js/list-sort.js"></script>
     
   <script type="text/javascript">
   $(function() {
