8330292a4f31428197ee89e2ac48f3cbb98c5c92
Nathan Pelletier <npelleti@ualberta.ca>
2023-01-10 20:32:18 -0700

    Created basic structure methods & functions

--- 2023/list sort/list-sort/list-sort.js
+++ 2023/list sort/list-sort/list-sort.js
@@ -11,3 +11,27 @@
  *    Probably change class to class = "sort-up"
  *    3rd click on same element returns list to generic sorting based on data.
 */
+
+//Classes and Functions//
+class ListSort{
+    constructor(table){
+        //table needs to remain unchanged while the html changes
+        this.table = table;
+    }//constructor
+
+    //initial click to sort down
+
+    //sort-down to sort-up
+
+    //sort-up to return to initial sort
+}//ListSort
+
+
+
+//EVENTS//
+
+//onclick table header without class
+
+//onclick table header with sort-down class
+
+//onclick table header with sort-up class
