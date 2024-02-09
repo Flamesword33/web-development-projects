6d97cf02f0ab08e668fce4eddb8527abf85c65f0
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-02-09 15:50:28 -0700

    Re-planned list-sort.js
    Added plans for onclick and class with each state change
    
    Added how I would keep the data

--- 2023/list sort/list-sort/list-sort.js
+++ 2023/list sort/list-sort/list-sort.js
@@ -5,11 +5,17 @@
  * 
  * Little idea for a stand alone library:
  *   When user clicks on a class="sortable" element the list reforms, sorted alphabetically.
- *    Probably changes class to class = "sort-down"
+ *    Change clicked <hr> 
+ *      class = "sort-down" onclick = "sort-up"
+ *      class = "sort-up"   onclick = "revert-sort"
+ *      
  *    Change all other classes of sort-down or sort-up to sortable
  *    If the user clicks again the list sorts reverse alphabetically.
  *    Probably change class to class = "sort-up"
  *    3rd click on same element returns list to generic sorting based on data.
+ * 
+ *   Storage
+ *     2 matrix's, one remembers initial sorting and one is sorted based on column
 */
 
 
