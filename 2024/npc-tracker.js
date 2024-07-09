a3978f96b4389bdb0557a114a6f2a927f5e75ad7
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-07-08 20:56:15 -0600

    npc-tracker alpha complete
    Fixed error where roll_dice returned the dice face count.
    Fixed multiple values in string form, they are now int's

--- 2024/npc-tracker.js
+++ 2024/npc-tracker.js
@@ -38,24 +38,33 @@
         dice : type of dice to be rolled (string values: none, d4, d6, d8, d10, d12, d20, d100)
 */
 
-/** attack()
- *  scrubs page for battle data using id's
- *  Deligates tasks, rolling to hit and rolling damage
- *  Outputs a final result to page
+
+function roll(dice){
+    if(dice == 0)
+        return 0;
+    else{
+        let roll = Math.floor(Math.random() * Number(dice)) + 1;
+        return roll;
+    }       
+}//roll
+
+/** roll_dice()
+ * rolls multiple dice and returns the result + the modifier
+ * @param {int} number_of_dice 
+ * @param {string} dice 
+ * @param {int} modifier 
+ * @returns {int}
  */
-function attack(){
-    //Int
-    const hit_mod = document.getElementById("hitmod").innerHTML;
-    const dmg_mod = document.getElementById("dmgmod").innerHTML;
-    const num_dice = document.getElementById("numdice").innerHTML;
-    //String
-    const dice = document.getElementById("dice").innerHTML; 
+function roll_dice(number_of_dice, dice, modifier){
+    let result = Number(modifier);
+
+    for(let x = 0; x < Number(number_of_dice); x++){
+        //window.alert(String(x));
+        result = result + roll(dice);
+    }
     
-    const roll_to_hit = roll_to_hit(hit_mod);
-    const damage = roll_damage(dmg_mod, num_dice, dice);
-    
-    display_result(roll_to_hit, damage);
-}//attack
+    return result;
+}//roll_dice
 
 
 /** roll_to_hit()
@@ -66,37 +75,40 @@ function attack(){
 function roll_to_hit(hitmod){
     //should roll a number between 1 & 20
     let d20 = Math.floor(Math.random() * 20) + 1;
-
-    if(typeof(hitmod) == typeof(0)){
-        let result = d20 + hitmod;
-        return result;
-    }//if hitmod is a number
-
-    else{
-        return d20;
-    }//else hitmod is not a number
+    let result = d20 + Number(hitmod);
+    return result;
 }//roll_to_hit
 
-/** roll_dice()
- * rolls multiple dice and returns the result + the modifier
- * @param {int} number_of_dice 
- * @param {string} dice 
- * @param {int} modifier 
- * @returns {int}
- */
-function roll_dice(number_of_dice, dice, modifier){
-    if(typeof(number_of_dice) != typeof(0))
-        number_of_dice = 1;
-    if(dice == "none")
-        dice = 0;
-    if(typeof(modifier) != typeof(0))
-        modifier = 0;
-    dice = determine_dice(dice);
-    let result = modifier;
 
-    for(let x = 0; x < number_of_dice; x++){
-        result = roll(dice) + result;
-    }
+/** attack()
+ *  scrubs page for battle data using id's
+ *  Deligates tasks, rolling to hit and rolling damage
+ *  Outputs a final result to page
+ */
+function attack(){
+    //Int
+    let hit_mod = document.getElementById("hitmod").value;
+    if(hit_mod == "")
+        hit_mod = 0;
+    let dmg_mod = document.getElementById("dmgmod").value;
+    if(dmg_mod == "")
+        dmg_mod = 0;
+    let num_dice = document.getElementById("numdice").value;
+    if(num_dice == "")
+        num_dice = 1;
+    let dice = document.getElementById("dice").value; 
+
+    let text1 = "";
+    //window.alert(text1.concat(String(hit_mod),"-",String(dmg_mod),"-", String(num_dice), "-", String(dice)));
     
-    return result;
-}//roll_damage
+    let hit = roll_to_hit(hit_mod);
+    let damage = roll_dice(num_dice, dice, dmg_mod);
+    //window.alert(text1.concat(String(hit), "-", String(damage)));
+    
+    display_result(hit, damage);
+}//attack
+
+function display_result(hit, damage){
+    let text1 = "";
+    window.alert(text1.concat(hit, " to hit. \n", damage, " damage."));
+}
