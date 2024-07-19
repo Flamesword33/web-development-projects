37a9f008e268f812d344ddfd327bbcdcfb480823
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-07-19 11:24:24 -0600

    Started dice-roller project
    Started basic documentation for css, javascript and html
    Linked html to css and javascript
    Created project skeleton jpg

---
+++ 2024/dice-roller/dice-roller.js
@@ -0,0 +1,10 @@
+/** dice-roller.html
+    by Nathan Pelletier
+    started July 19 2024
+
+    A program to roll a bunch of dice. It can roll a number of dice with a modifier.
+    It can roll advantage (2 dice take the higher), disadvantage (2 dice take the lower).
+    It can also roll 4 dice and drop the lowest.
+
+    Program will
+ */
