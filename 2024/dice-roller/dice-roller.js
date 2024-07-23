8bb6627a363cb5a8d47388b02075c4451fd58dfd
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-07-23 12:26:36 -0600

    Updated documentation

--- 2024/dice-roller/dice-roller.js
+++ 2024/dice-roller/dice-roller.js
@@ -6,5 +6,13 @@
     It can roll advantage (2 dice take the higher), disadvantage (2 dice take the lower).
     It can also roll 4 dice and drop the lowest.
 
-    Program will
+    File will
+      Display Dice & max number or number last rolled
+      Slider to select dice type
+        Only gives options for dice used in D&D
+        Should show dice at max number before roll
+      Logic for Roll button
+      Takes info from Drop menu
+        Prevent multiple buttons in drop menu
+      Populate result box
  */
