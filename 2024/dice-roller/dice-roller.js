8245a1f7bcb1a97f81d6332af1d890f4bef38584
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-07-23 21:06:21 -0600

    Created roll_dice(), basic_roll(), roll()
    program now rolls dice, records result on each step and outputs to screen

--- 2024/dice-roller/dice-roller.js
+++ 2024/dice-roller/dice-roller.js
@@ -1,4 +1,4 @@
-/** dice-roller.html
+/** dice-roller.js
     by Nathan Pelletier
     started July 19 2024
 
@@ -16,3 +16,27 @@
         Prevent multiple buttons in drop menu
       Populate result box
  */
+
+function roll_dice(){
+  let dice_type = Number(document.getElementById("dice-type").value);
+  let number_of_dice = Number(document.getElementById("how-many-dice").value);
+  //will eventually add result from drop menu here
+  basic_roll(dice_type, number_of_dice);
+}//roll_dice
+
+function basic_roll(dice_type, number_of_dice){
+  let current_number = roll(dice_type);
+  let final_number = current_number;
+  let result = String(current_number);
+  for(let x = 0; x < number_of_dice - 1; x++){
+    current_number = roll(dice_type);
+    result = result + " + " + String(current_number);
+    final_number = current_number + final_number;
+  }
+  result = result + " = " + String(final_number);
+  document.getElementById("roll-output").innerText = result;
+}
+
+function roll(dice){
+  return Math.floor(Math.random() * dice) + 1;
+}//roll
