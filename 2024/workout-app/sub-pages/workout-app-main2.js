39c2e5450a900986b39594c8f3f522a303af1457
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-07-31 14:22:09 -0600

    Seperated js file into 2 copies for main2 and machine
    Removed unnecessary js links

---
+++ 2024/workout-app/sub-pages/workout-app-main2.js
@@ -0,0 +1,28 @@
+/** workout-app.js
+    by Nathan Pelletier
+    started July 26 2024
+    
+    This app is a re-creation of an app I previously made over 2 weeks for a client.
+    In 2020 myself and Oscar were working for the Camrose Fitness Center.
+    We designed an app to welcome the user, introduce a user to the machines present
+      based on images. 
+    Each machine page included a picture of the machine, a video on how to use it and a further discription.
+    Finally we included a link with google map data to show users how to get to the gym.
+    
+    Said original app is lost to time. All that remains are the assets I created for the endevor.
+    
+    This file will:
+      Allow for the various machines to display on workout-app-main2.html
+        based on id tag in url (hopefully don't need backend)
+        Display correct workout machienes
+        Hide incorrect machienes
+
+    https://stackoverflow.com/questions/1034621/get-the-current-url-with-javascript  //link to get current url with js
+    window.location.href
+    https://stackoverflow.com/questions/36858508/how-to-send-a-parameter-in-html-link-and-how-to-retrieve-it //retrieve url info
+    var params = {};
+    location.search.slice(1).split("&").forEach(function(pair) {
+      pair = pair.split("=");
+      params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    });
+*/
