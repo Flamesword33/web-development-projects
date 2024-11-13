1c81d2fb9ca26bccc2ab48d09740460c586008ad
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-11-13 14:03:44 -0700

    Added canvas from scrolling background tutorial
    Got scrolling background to work but all other stage actors are now covered. Also their execution was buggy. By drawing a 2d image before the page is drawn a Null pointer error was returned.

--- 2024/dino-game/dino-game.js
+++ 2024/dino-game/dino-game.js
@@ -17,7 +17,67 @@
     
 */
 
+ 
 
-window.onload = function() {
+ 
+
+window.addEventListener('keypress', function(event){
+  if(event.code == "Space"){
+    scroll_screen();
+  }
+})
+
+function scroll_screen(){
+  var can = document.getElementById('canvas1');    
   
-};
+  // the initial image height
+  var img_width = 0;
+  var height = can.height;
+  var width = can.width;
+ 
+  // the scroll speed
+  // an important thing to ensure here is that can.height
+  // is divisible by scrollSpeed
+  var scrollSpeed = 2;
+
+  // The 2D Context for the HTML canvas element. It
+  // provides objects, methods, and properties to draw and
+  // manipulate graphics on a canvas drawing surface.
+  var ctx = can.getContext('2d');
+   
+  // create an image element
+  var img = new Image();
+   
+  // specify the image source relative to the html or js file
+  // when the image is in the same directory as the file
+  // only the file name is required:
+  img.src = "images/dino\ game\ background.jpg";
+   
+      // this is the primary animation loop that is called 60 times
+      // per second
+      function loop()
+      {
+          // draw image 1
+          ctx.drawImage(img, img_width, 0, width, height);
+   
+          // draw image 2
+          ctx.drawImage(img, img_width - width, 0, width, height);
+   
+          // update image height
+          img_width += scrollSpeed;
+   
+          //resetting the images when the first image entirely exits the screen
+          if (img_width == width)
+              img_width = 0;
+   
+          // this function creates a 60fps animation by scheduling a
+          // loop function call before the
+          // next redraw every time it is called
+          window.requestAnimationFrame(loop);
+      }
+   
+      // this initiates the animation by calling the loop function
+      // for the first time
+      loop();
+}
+
