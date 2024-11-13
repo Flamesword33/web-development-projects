c99faa4b36e8432b7ceb5384d9058c0148e842c9
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-11-13 13:59:41 -0700

    Updated project comment

--- 2024/dino-game/dino-game.css
+++ 2024/dino-game/dino-game.css
@@ -2,13 +2,13 @@
     by Nathan Pelletier
     Started November 12 2024
     
-  A copy of the crome out of internet dino-game. Made with the tutorial:
+  A copy of the crome out of internet dino-game. Made with the tutorials:
   "https://youtu.be/4Oz34co7VLY"
+  https://www.geeksforgeeks.org/html5-game-development-infinitely-scrolling-background/
   At end dino can:
     run
     jump
-      over rocks
-      over cactus
+      over trees
     duck
       under birds
   Score counts up based on timer * 10
