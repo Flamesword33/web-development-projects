51bf90678211ccdab578969539f2c20e793bf679
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-12-02 13:18:53 -0700

    Comment work

--- 2024/dino-game/dino-game.js
+++ 2024/dino-game/dino-game.js
@@ -15,12 +15,26 @@
   Score counts up based on timer * 10
 
   This file will:
-    
+    Draw the
+      background
+      dino
+      tree
+      bird
+    animate
+      Dino
+        jump
+        run
+        duck
+      background
+        scroll
+        parallax?
+      bird/tree
+        move towards dino
+    score
+      distance travelled
+    end game
+      if dino connects with tree or bird
 */
-
- 
-
- 
 
 window.addEventListener('keypress', function(event){
   if(event.code == "Space"){
@@ -33,7 +47,6 @@ function scroll_screen(){
   
   //starting img_width at width and going to 0 makes right to left camera
   //starting img_width at 0 and increasing to width makes left to right camera
-  // the initial image height
   var height = can.height;
   var width = can.width;
   var img_width = width;
@@ -56,31 +69,29 @@ function scroll_screen(){
   // only the file name is required:
   img.src = "images/dino\ game\ background.jpg";
    
-      // this is the primary animation loop that is called 60 times
-      // per second
-      function loop()
-      {
-          // draw image 1
-          ctx.drawImage(img, img_width, 0, width, height);
+  // this is the primary animation_loop that is called 60 times
+  // per second
+  function animation_loop(){
+    // draw image 1
+    ctx.drawImage(img, img_width, 0, width, height);
    
-          // draw image 2
-          ctx.drawImage(img, img_width - width, 0, width, height);
+    // draw image 2
+    ctx.drawImage(img, img_width - width, 0, width, height);
    
-          // update image height
-          img_width -= scrollSpeed;
+    // update image height
+    img_width -= scrollSpeed;
    
-          //resetting the images when the first image entirely exits the screen
-          if (img_width == 0)
-              img_width = width;
+    //resetting the images when the first image entirely exits the screen
+    if (img_width == 0)
+      img_width = width;
    
-          // this function creates a 60fps animation by scheduling a
-          // loop function call before the
-          // next redraw every time it is called
-          window.requestAnimationFrame(loop);
-      }
+    // this function creates a 60fps animation by scheduling a
+    // animation_loop function call before the
+    // next redraw every time it is called
+    window.requestAnimationFrame(animation_loop);
+  }//animation_loop
    
-      // this initiates the animation by calling the loop function
-      // for the first time
-      loop();
-}
+  // initiates the animation
+  animation_loop();
+}//scroll_screen()
 
