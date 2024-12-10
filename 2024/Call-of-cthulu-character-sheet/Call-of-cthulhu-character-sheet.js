d89115dc345fbab9e2ea1006b0d3af888ed0f24e
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-12-10 15:04:40 -0700

    Started Call of Cthulhu character sheet.
    Added basic headers and skeleton
    
    CSS --> lightgrey to save my eyes

---
+++ 2024/Call-of-cthulu-character-sheet/Call-of-cthulhu-character-sheet.js
@@ -0,0 +1,15 @@
+/*Call-of-cthulhu-character-sheet.html
+    by Nathan Pelletier
+    Started December 10 2024
+    
+  Takes the logic from Call of Cthulhu NPC generater.c to fill 
+  out a character sheet.
+
+  This file will:
+*/
+
+window.addEventListener('keypress', function(event){
+  if(event.code == "Space"){
+  }
+})
+
