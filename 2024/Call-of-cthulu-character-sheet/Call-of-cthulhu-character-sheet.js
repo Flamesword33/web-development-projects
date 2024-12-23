6ac0def30a599ee49df6657b1b415cd3b929425d
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-12-23 13:04:29 -0700

    Finished and used meta print
    Used template
      <label for="data" class="box4">data</label>
      <input type="tel" class="box4 data primary" id="data">
      <input type="tel" class="box4 data secondary">
      <input type="tel" class="box4 data third">
    
    Implemented 8 core stats (need to fix to box2 instead of 4)
    Added 1 derived stat
    Added 45 specific stats

--- 2024/Call-of-cthulu-character-sheet/Call-of-cthulhu-character-sheet.js
+++ 2024/Call-of-cthulu-character-sheet/Call-of-cthulhu-character-sheet.js
@@ -323,7 +323,13 @@ function meta_print(data){
   <label for="data" class="box4">data</label>
   <input type="tel" class="box4 data primary" id="data">
   <input type="tel" class="box4 data secondary">
-  <input type="tel" class="box4 data third"></input>*/
+  <input type="tel" class="box4 data third">*/
 
+  //Concatination in Javascript: either use variable.concat(), + or , 
+  //  concatinating with a , adds a space to the final string
+  console.log("<label for=\"" + data + "\" class=\"box4\">" + data + "</label> \n");
+  console.log("<input type=\"tel\" class=\"box4 data primary\" id=\"" + data + "\"> \n");
+  console.log("<input type=\"tel\" class=\"box4 data secondary\"> \n");
+  console.log("<input type=\"tel\" class=\"box4 data third\"> \n \n");
 
 }
